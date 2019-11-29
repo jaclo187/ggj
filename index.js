c1cc7705756337e4fc7fa32b0f1a08ace072b565
Louis <jaclo187@school.lu>
2019-11-29 11:14:15 +0100

    Added helmet middleware - adds nosniff header

--- index.js
+++ index.js
@@ -5,8 +5,17 @@ const PORT = 8080
 const express = require('express')
 const http = require('http')
 const app = express()
+const helmet = require('helmet')
 
+app.use(helmet.noSniff())
 app.use(express.static('public'));
+
+const logger = (req, res, next) => {
+    console.log("Loaded")
+    next()
+} 
+
+if(DEBUG) app.use(logger)
 
 const server = http.createServer(app);
 
