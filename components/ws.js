82b3ef1ecc40f76ab2fd1bbe9f69022b2ea4818e
Louis <jaclo187@school.lu>
2019-11-29 11:40:03 +0100

    Created websocket server class

---
+++ components/ws.js
@@ -0,0 +1,11 @@
+module.exports = class WebSocketServer{
+
+
+    constructor(server, session){
+        const WebSocketServer = require('ws').Server;
+        const wss = new WebSocketServer({server});
+
+        
+    }
+
+}
