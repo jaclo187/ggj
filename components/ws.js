a117eb15c3aea314354272dd0e9a1914c23ff7e2
Louis <jaclo187@school.lu>
2019-12-02 13:08:03 +0100

    Added Websocket Server on connect response

--- components/ws.js
+++ components/ws.js
@@ -1,11 +1,31 @@
 module.exports = class WebSocketServer{
 
-
     constructor(server, session){
         const WebSocketServer = require('ws').Server;
         const wss = new WebSocketServer({server});
+        const conn = require('./db');
+        const bcrypt = require('bcryptjs');
 
+        wss.on('connection', (socket, req) => {
+            const send = (msg, options) => {
+              const obj = {};
+              Object.assign(obj, {msg: msg}, options);
+              socket.send(JSON.stringify(obj));
+            };
+      
+            session(req, {}, ()=> {
+              if(req.session && req.session.loggedin) send('login',{success: true});
+            });
+      
+            socket.on('error', err => {
+              console.dir(err);
+            });
+
+            console.log("WSS INITIALIZED")
+            console.log(req);
+
+        })
         
     }
 
-}
+};
