e1e4dc6fee966ca61d322a65c0cec6d91690cdd4
Louis <jaclo187@school.lu>
2019-12-02 13:05:41 +0100

    Server updated - added express sessions - added mysql session store - configured sessions - added websocket server

--- index.js
+++ index.js
@@ -6,9 +6,30 @@ const express = require('express')
 const http = require('http')
 const app = express()
 const helmet = require('helmet')
+const session = require('express-session');
+const MySQLStore = require('express-mysql-session')(session);
+const MySQLSessionOpts = require(__dirname + '/components/mysql_options.js').mysqlSessionOpts;
+const sessionStore = new MySQLStore(MySQLSessionOpts);
+const sessionOpts = {
+    cookie: {
+      path: '/', //nodejaclo/',
+      domain: "192.168.132.238",
+      secure: false,
+      expires: new Date(Date.now() + 86400000),
+      httpOnly: true 
+    },
+    secret: 'moxqksyo',
+    resave: false,
+    saveUninitialized: true,
+    name: 'test1.sid',
+    key: 'globalgamejam',
+    store: sessionStore,
+    secure: true
+};
+const sess = session(sessionOpts);
 
 app.use(helmet.noSniff())
-app.use(express.static('public'));
+app.use('/', express.static('public'));
 
 const logger = (req, res, next) => {
     console.log("Loaded")
@@ -18,5 +39,8 @@ const logger = (req, res, next) => {
 if(DEBUG) app.use(logger)
 
 const server = http.createServer(app);
+
+const WebSocketServer = require('./components/ws.js')
+const wss = new WebSocketServer(server, sess);
 
 server.listen(PORT);
