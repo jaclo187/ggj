5cecac1786fcd08871595d8f50bb0877dafe6739
Louis <jaclo187@school.lu>
2020-01-10 12:13:29 +0100

    Added checks and toggle method - Checks if values are set or not - Added method to toggle classes of hidden container

--- public/js/participant.js
+++ public/js/participant.js
@@ -11,15 +11,24 @@ class WebSocketClient {
         this.wss.addEventListener('open', async () => {
             console.log("WSS Opened")
 
+            document.querySelector('#team').addEventListener('change', async e => {
+                e.preventDefault();
+                if(e.target.value === "create"){
+                    document.querySelector('#form-create-team').classList.remove('d-none');
+                    document.querySelector('#form-join-team').classList.add('d-none');
+                } else {
+                    document.querySelector('#form-create-team').classList.add('d-none');
+                    document.querySelector('#form-join-team').classList.remove('d-none');
+                }
+            });
+
             document.querySelector('#submit-form').addEventListener('click', async e => {
                 e.preventDefault();
-                console.log("update trigger")
                 let data = this.getFormData();
-                console.log(data)
                 await this.execute('updateUser', data, 'updateUser');
             });
 
-            await this.execute('getUserData', {}, 'getUserData');
+            await this.execute('updateTeams', {}, 'updateTeams');
         });
 
         this.wss.addEventListener('message', e => {
@@ -41,7 +50,10 @@ class WebSocketClient {
                         this.passUserData(data);
                         break;
                     case "updateUser": 
-                        this.onUpdate(data)
+                        this.onUpdate(data);
+                        break;
+                    case "updateTeams":
+                        this.fillTeamSelect(data);
                         break;
                     default:
                         break;
@@ -60,6 +72,7 @@ class WebSocketClient {
      * @param {Object} options an object containing the actual infromation
      * @param {String} msg Message for the promise object
      */
+        console.log(command);
         return new Promise((resolve, reject) => {
             this.promises.push({msg: msg, resolve: resolve});
             let obj = {};
@@ -91,31 +104,40 @@ class WebSocketClient {
     passUserData(data) {
         data = data[0]
 
-        document.querySelector('#size').value = data.dtTShirtSize;
-        document.querySelector('#food').value = data.dtFood;
-        document.querySelector('#allergies').value = data.dtAllergies;
+        if(data.dtTShirtSize !== null)
+            {document.querySelector('#size').value = data.dtTShirtSize;}
+        if(data.dtFood !== null)
+            {document.querySelector('#food').value = data.dtFood;}
+        if(data.dtAllergies !== null)
+            {document.querySelector('#allergies').value = data.dtAllergies;}
 
         if(data.dtName === null){
             document.querySelector('#team').selectedIndex = 1
         } else {
             document.querySelector('#team').selectedIndex = 0
+            document.querySelector('#join-team').innerHTML += `<option selected>${data.dtName}</option>`;
         }
-
-        document.querySelector('#join-team').innerHTML += `<option selected>${data.dtName}</option>`;
-        document.querySelector('#hardware').value = data.dtDescription;
-        document.querySelector('#watt').value = data.dtPower;
-        document.querySelector('#outlets').value = data.dtOutlets;
+        if(data.dtDescription !== null)
+            {document.querySelector('#hardware').value = data.dtDescription;}
+        if(data.dtPower !== null)
+            {document.querySelector('#watt').value = data.dtPower;}
+        if(data.dtOutlets !== null)
+            {document.querySelector('#outlets').value = data.dtOutlets;}
+        
     }
 
     fillTeamSelect(data){
         let e = document.querySelector('#join-team'),
         html = "";
+
         for(let key in data){
-            if(data.hasOwnProperty(key)){
-                html += `<option value='${data[key]}'>${data[key]}</option>`;
+            
+            if(data.hasOwnProperty(key) && data[key].dtName !== undefined){
+                html += `<option value='${data[key].dtName}'>${data[key].dtName}</option>`;
             }
         }
         e.innerHTML += html;
+        this.execute('getUserData', {}, 'getUserData');
     }
   
 };
