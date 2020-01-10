1bae097bf93fcf9c0c3fa978a3be50164992a296
Louis <jaclo187@school.lu>
2020-01-10 12:12:08 +0100

    Displaying Users in Admin Panel

--- public/js/admin.js
+++ public/js/admin.js
@@ -1,11 +1,21 @@
 'use strict';
+
+let wsc;
+
 class Admin{
+
     constructor(){
         const wssURL = `ws://${location.host}`; 
         this.wss = new WebSocket(wssURL);
         this.promises = []; //contains promises send to the server
-        this.wss.addEventListener('open', () => {
-            await this.execute('getLocation', data, 'getLocation');
+        this.wss.addEventListener('open', async () => {
+            console.log("WSS Opened")
+
+            document.querySelector('#users-container-button').addEventListener('click', async e => {
+                e.preventDefault()
+                await this.execute('getUsers',{},'getUsers');
+            });
+
         });
 
         this.wss.addEventListener('message', e => {
@@ -25,11 +35,14 @@ class Admin{
           
                 //main websocket message structure
                 switch (data.msg){  
-                    case "logout":
-                        this.onLogout(data)
+                    case "logout" :
+                        this.onLogout(data);
                         break;
-                    case "getLocation":
+                    case "getLocation" :
                         this.fillSelect("ipt-select-location",data.locations);
+                        break;
+                    case "getUsers" :
+                        this.fillUsers(data);
                         break;
                     default:
                         break;
@@ -49,6 +62,7 @@ class Admin{
          * @param {Object} options an object containing the actual infromation
          * @param {String} msg Message for the promise object
          */
+        console.log(command);
             return new Promise((resolve, reject) => {
                 this.promises.push({msg: msg, resolve: resolve});
                 let obj = {};
@@ -71,10 +85,44 @@ class Admin{
         }
 
     }
+
+    fillUsers (data) {
+        data
+        let element = document.querySelector('#users-container-body'),
+        node;
+        element.innerHTML = "";
+        if(data){
+            for(let key in data){
+                if(data.hasOwnProperty(key)){
+                    node = 
+                    `<div class='card card-body'>
+                        <p>Email: ${data[key].dtEmail}</p>
+                        <p>First Name: ${data[key].dtFirstName}</p>
+                        <p>Last Name: ${data[key].dtLastName}</p>
+                        <p>Food preferences: ${data[key].dtFood}</p>
+                        <p>Allergies: ${data[key].dtAllergies}</p>
+                        <p>Payment Status: ${data[key].dtHasPaied}</p>
+                        <p>Skill Set: ${data[key].dtSkillSet}</p>
+                        <p>T-Shirt Size: ${data[key].dtTShirtSize}</p>
+                        <p>Group: ${data[key].dtName}</p>
+                        <button type="button" class="btn btn-danger" onclick="wsc.onUserDelete(${data[key].idPerson});">Delete</button>
+                    </div>`;
+                    element.innerHTML += node;
+                }
+            }
+        }
+    }
+
+    onUserDelete(user){
+        this.execute('deleteUser',{userID : user},'deleteUser');
+        window.location.reload();
+    }
     
     onLogout (data) {
         wsc.updateNav()
         window.location.reload()
     }
     
-}
+};
+
+addEventListener('DOMContentLoaded', wsc = new Admin);
